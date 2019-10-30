4105bf7120a05f808847dac24a7eafeb3ee01ecd
Ashraf-Raneem <you@example.com>
2019-10-29 19:54:30 -0700

    task1

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 /* Add your script here */
+//task 1
+$("#list").click(function(){
+    $.ajax({
+        method: 'GET',
+        url: 'http://skeleton.mernbook.com/api/users'
+       }).done(function(response){
+        response.forEach(element =>{
+            let user = $('<p>').text(element.name);
+            $('#users').append(user)
+        });
+       }).fail(function(response){
+        console.log(response);
+       });
+       
+})
